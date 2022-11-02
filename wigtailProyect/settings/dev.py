ae1a114605c80b69f619c5e8c0eccd3748b95879
freddyloayza <freddyloayza89@gmail.com>
2022-11-01 21:48:46 -0500

    Creado el proyecto inicial

---
+++ wigtailProyect/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-p4&q3kp=68hvoh3gdsugq2v$#5bxpq9)qxtzjf3nkn8kz16v)="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
